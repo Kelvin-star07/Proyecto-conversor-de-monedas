6f46c99553916dcfdc81f6091586c4f54564720f
kelvin <kelvinkodaddiaz@gmail.com>
2025-05-05 14:30:18 -0400

    Conversor de menedas en proceso

---
+++ src/proyecto/conversor/de/monedas/Principal.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyecto.conversor.de.monedas;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Principal {
+    
+     public static void main(String[] args) {
+        
+         
+         
+         
+         
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
